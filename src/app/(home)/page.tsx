64cc11f6b2bc5d2d42446be43c95ad4f1fca76eb
Nhatcapdang <54816813+Nhatcapdang@users.noreply.github.com>
2026-02-05 17:56:13 +0700

    dsa

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import ParallaxScroll1 from '@/components/scroll/parallax-scroll-1';
 import Link from 'next/link';
 
 export default function HomePage() {
@@ -11,6 +12,26 @@ export default function HomePage() {
         </Link>{' '}
         and see the documentation.
       </p>
+      <div className="h-screen bg-amber-400">
+        <div className="h-full bg-blue-200 p-4 text-center items-end flex">
+          <h1>Lorem ipsum dolor sit amet</h1>
+        </div>
+      </div>
+      <ParallaxScroll1
+        className="h-calc(100vh-57px)"
+        offset={['start center', 'center start']}
+      >
+        <div className="relative w-full h-full rounded-xl overflow-hidden grid grid-cols-4  gap-4 liquid-glass p-4">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <div
+              key={index}
+              className="relative w-full h-full rounded-xl overflow-hidden aspect-square"
+            >
+              Lorem ipsum dolor sit amet
+            </div>
+          ))}
+        </div>
+      </ParallaxScroll1>
     </div>
   );
 }
--- src/components/scroll/parallax-scroll-1.tsx
+++ src/components/scroll/parallax-scroll-1.tsx
@@ -1,37 +1,49 @@
-'use client'
+'use client';
 
-import { motion, useScroll, useSpring, useTransform } from 'motion/react'
-import { useRef } from 'react'
-
+import { cn } from '@/utils/cn';
+import { motion, useScroll, useSpring, useTransform } from 'motion/react';
+import { useRef } from 'react';
 
 interface ParallaxScroll1Props {
-    offset?: ['start center', 'center start'] | ['center', 'center'],
-    children: React.ReactNode
+  offset?: ['start center', 'center start'] | ['center', 'center'];
+  children: React.ReactNode;
+  className?: string;
 }
-export default function ParallaxScroll1({ offset = ['center', 'center'], children }: ParallaxScroll1Props) {
-    const ref = useRef(null)
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset,
-    })
+export default function ParallaxScroll1({
+  offset = ['center', 'center'],
+  children,
+  className,
+}: ParallaxScroll1Props) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset,
+  });
 
-    const y = useTransform(scrollYProgress, [0, 1], ['10%', '0%'])
-    const ySpring = useSpring(y, {
-        stiffness: 100,
-        damping: 10,
-        mass: 1,
-    })
+  const y = useTransform(scrollYProgress, [0, 1], ['15%', '-5%']);
+  const ySpring = useSpring(y, {
+    stiffness: 100,
+    damping: 10,
+    mass: 1,
+  });
 
-    return (
-        <div ref={ref} className="h-[90vh] relative overflow-hidden">
-            <h1 className="md:text-8xl xs:text-6xl text-4xl font-bold text-center">NHATCAPDANG</h1>
-            <motion.div
-                style={{ y: ySpring }}
-                className="h-[82vh] absolute top-0 inset-x-0 z-10"
-            >
-                {children}
-            </motion.div>
-            <h1 className="md:text-8xl xs:text-6xl text-4xl font-bold text-center absolute bottom-0 left-0 right-0">NHATCAPDANG</h1>
-        </div>
-    )
+  return (
+    <div
+      ref={ref}
+      className={cn('relative h-[90vh]  overflow-hidden', className)}
+    >
+      <h1 className="md:text-8xl xs:text-6xl text-4xl font-bold text-center">
+        NHATCAPDANG
+      </h1>
+      <motion.div
+        style={{ y: ySpring }}
+        className="h-[82vh] absolute top-0 inset-x-0 z-10"
+      >
+        {children}
+      </motion.div>
+      <h1 className="md:text-8xl xs:text-6xl text-4xl font-bold text-center absolute bottom-0 left-0 right-0">
+        NHATCAPDANG
+      </h1>
+    </div>
+  );
 }
